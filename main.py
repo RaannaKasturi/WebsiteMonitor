bb4f94f420bc3f60cda32725f3d934b6488c0723
Nayan Kasturi <raannakasturi@gmail.com>
2024-06-26 16:18:38 +0000

    chore: Refactor getScreenshot.py and getStatus.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from getStatus import getStatus
 from getScreenshot import getScreenshot
 from saveDataSendMail import saveDataSendMail
 import subprocess
+import sys
 
 def cleanURL(inputURL):
     url = tldextract.extract(inputURL)
@@ -22,15 +23,18 @@ def getData(url, email):
     return domain, URL, code, status, webStatus, moreDetails, img, imgurl, email, downcount
 
 def installGC():
-    subprocess.run(['apt-get', 'update'])
-    subprocess.run(['apt-get', 'install', '-y', 'wget', 'unzip'])
-    subprocess.run(['wget', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['apt-get', 'install', '-y', './google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['rm', 'google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['apt-get', 'clean'])
-    subprocess.run(['sudo', 'apt-get', 'update'])
-    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'wget', 'unzip'])
-    subprocess.run(['wget', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['sudo', 'apt-get', 'install', '-y', './google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['sudo', 'rm', 'google-chrome-stable_current_amd64.deb'])
-    subprocess.run(['sudo', 'apt-get', 'clean'])
+    if sys.platform == 'linix':
+        subprocess.run(['apt-get', 'update'])
+        subprocess.run(['apt-get', 'install', '-y', 'wget', 'unzip'])
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['apt-get', 'install', '-y', './google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['rm', 'google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['apt-get', 'clean'])
+        subprocess.run(['sudo', 'apt-get', 'update'])
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'wget', 'unzip'])
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', './google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['sudo', 'rm', 'google-chrome-stable_current_amd64.deb'])
+        subprocess.run(['sudo', 'apt-get', 'clean'])
+    else:
+        subprocess.run(['powershell', '-command', 'winget', 'install', 'Google.Chrome', '--force'])
